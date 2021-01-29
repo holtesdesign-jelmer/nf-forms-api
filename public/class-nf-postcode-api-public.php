49b36511636d31c011242887def05e307341d168
Nils Ringersma <nilsringersma@gmail.com>
2021-01-29 10:11:35 +0100

    feat: add loading value

--- public/class-nf-postcode-api-public.php
+++ public/class-nf-postcode-api-public.php
@@ -20,82 +20,73 @@
  * @subpackage NF_Postcode_Api/public
  * @author     Holtes Design <email@example.com>
  */
-class NF_Postcode_Api_Public {
+class NF_Postcode_Api_Public
+{
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $nf_postcode_api    The ID of this plugin.
-	 */
-	private $nf_postcode_api;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $nf_postcode_api The ID of this plugin.
+     */
+    private $nf_postcode_api;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $version The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $nf_postcode_api       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $nf_postcode_api, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @param string $nf_postcode_api The name of the plugin.
+     * @param string $version The version of this plugin.
+     * @since    1.0.0
+     */
+    public function __construct($nf_postcode_api, $version)
+    {
+        $this->nf_postcode_api = $nf_postcode_api;
+        $this->version = $version;
+    }
 
-		$this->nf_postcode_api = $nf_postcode_api;
-		$this->version = $version;
-
-	}
-
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in NF_Postcode_Api_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The NF_Postcode_Api_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->nf_postcode_api, plugin_dir_url( __FILE__ ) . 'css/nf-postcode-api-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in NF_Postcode_Api_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The NF_Postcode_Api_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-	}
+    /**
+     * Register the JavaScript for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts()
+    {
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in NF_Postcode_Api_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The NF_Postcode_Api_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
+        wp_enqueue_script(
+            'ajax-script',
+            plugin_dir_url(__FILE__) . 'js/nf-postcode-api-public.js',
+            array('nf-front-end'),
+            "1.0",
+            true
+        );
+        wp_localize_script(
+            'ajax-script',
+            'nf_ajax_url',
+            array(
+                'ajaxurl' => admin_url('admin-ajax.php'), // URL to WordPress ajax handling page
+                'nonce' => wp_create_nonce('nf_postcode_api'),
+            )
+        );
+    }
 
 }
--- public/js/nf-postcode-api-public.js
+++ public/js/nf-postcode-api-public.js
@@ -1,13 +1,13 @@
-(function( $ ) {
+(function ($) {
 
-    var formExists = setInterval(function() {
+    var formExists = setInterval(function () {
 
         if ($(".nf-form-cont").length) {
 
-            $('.api-postcode, .api-house_number, .api-suffix' ).on("focusout",  function(){
+            $('.api-postcode, .api-house_number, .api-suffix').on("focusout", function () {
                 getPostcodeAndNumber();
             });
-            $('.api-postcode, .api-house_number').on("input",  function(){
+            $('.api-postcode, .api-house_number').on("input", function () {
                 //getPostcodeAndNumber();
             });
             clearInterval(formExists);
@@ -28,25 +28,29 @@
         var house_number = $('.api-house_number').val();
         var house_number_suffix = $('.api-suffix').val();
 
-        if ( $('.nf-form-cont').parent().find( '.postcode-api-error' ).length < 1 ) {
-            $( '.api-postcode' ).closest( '.nf-field-container' ).before( '<div class="postcode-api-error" style="color: red;"></div>' );
+        if ($('.nf-form-cont').parent().find('.postcode-api-error').length < 1) {
+            $('.api-postcode').closest('.nf-field-container').before('<div class="postcode-api-error" style="color: red;"></div>');
         }
 
         var data = {
-            security:            nf_ajax_url.nonce,
-            postcode:            postcode,
-            house_number:        house_number,
+            security: nf_ajax_url.nonce,
+            postcode: postcode,
+            house_number: house_number,
             house_number_suffix: house_number_suffix,
         };
 
-        if (postcode.length > 5 && house_number.length > 0 ) {
+        if (postcode.length > 5 && house_number.length > 0) {
             // Call Postcode API
             xhr = $.ajax({
-                type:		'POST',
-                url:		nf_ajax_url.ajaxurl+'?action=nf_postcode_api_request',
-                data:		data,
-                dataType:   'json',
-                success:	function( data ) {
+                type: 'POST',
+                url: nf_ajax_url.ajaxurl + '?action=nf_postcode_api_request',
+                data: data,
+                dataType: 'json',
+                beforeSend: function () {
+                    $('.api-street_name').val('Bezig met ophalen...');
+                    $('.api-city').val('Bezig met ophalen...');
+                },
+                success: function (data) {
                     if (!data) {
                         console.log('nonce check failed');
                         return;  // nonce check failed
@@ -56,10 +60,10 @@
                     console.log(JSON.stringify(data));
 
                     // If address is valid
-                    if ( typeof data.street != 'undefined' && typeof data.city != 'undefined') {
+                    if (typeof data.street != 'undefined' && typeof data.city != 'undefined') {
 
                         // Remove error and set fields to validated
-                        $('.postcode-api-error').html( '' );
+                        $('.postcode-api-error').html('');
 
                         // Fill address fields
                         $('.api-street_name').val(data.street);
@@ -67,38 +71,44 @@
 
                     }
                     // If address is not valid
-                    else if (typeof data.exceptionId != 'undefined' ) {
+                    else if (typeof data.exceptionId != 'undefined') {
+
+                        $('.api-street_name').val('');
+                        $('.api-city').val('');
+
                         // console.log(data);
-                        switch(data.exceptionId) {
+                        switch (data.exceptionId) {
                             case 'PostcodeNl_Controller_Address_PostcodeTooShortException':
-                                $( '.postcode-api-error' ).html( data.exception );
+                                $('.postcode-api-error').html(data.exception);
                                 break;
                             case 'PostcodeNl_Controller_Address_InvalidPostcodeException':
-                                $('.postcode-api-error' ).html( data.exception );
+                                $('.postcode-api-error').html(data.exception);
                                 break;
                             case 'PostcodeNl_Controller_Address_InvalidHouseNumberException':
-                                $('.postcode-api-error' ).html( data.exception );
+                                $('.postcode-api-error').html(data.exception);
                                 break;
                             case 'PostcodeNl_Service_PostcodeAddress_AddressNotFoundException':
-                                $( '.postcode-api-error' ).html( data.exception );
+                                $('.postcode-api-error').html(data.exception);
                                 break;
                             case 'PostcodeNl_Controller_Plugin_HttpBasicAuthentication_NotAuthorizedException':
                             case 'PostcodeNl_Controller_Plugin_HttpBasicAuthentication_PasswordNotCorrectException':
                                 // API credentials not valid - admin notice?
                                 break;
                             default:
-                                $('.postcode-api-error' ).html( data.exception );
+                                $('.postcode-api-error').html(data.exception);
                                 break;
                         }
+                    } else {
+                        $('.api-street_name').val('');
+                        $('.api-city').val('');
                     }
 
                 }
             });
-        }
-        else {
+        } else {
             console.log('Postcode te kort of huisnummer niet opgegeven');
         }
 
     }
 
-})( jQuery );
+})(jQuery);
